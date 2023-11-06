ab4807a1d429f240b517288e4eac9dddd2599852
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-11-06 15:06:33 +0100

    class templates - 2

--- class-templates/class_templates.cpp
+++ class-templates/class_templates.cpp
@@ -52,7 +52,7 @@ struct Array
 
     reference at(size_t index);
 
-    const_reference at(size_t index) const;
+    const_reference at(size_t index) const
     {
         if (index >= N)
             throw std::out_of_range("index out of range");
@@ -61,9 +61,13 @@ struct Array
     }
 };
 
-template<typename T, size_t N>
+// deduction guide
+template <typename T, typename... Ts>
+Array(T, Ts...) -> Array<T, sizeof...(Ts) + 1>;
+
+template <typename T, size_t N>
 typename Array<T, N>::reference Array<T, N>::at(size_t index)
-{ 
+{
     if (index >= N)
         throw std::out_of_range("index out of range");
 
@@ -79,4 +83,57 @@ TEST_CASE("class templates")
     std::cout << "\n";
 
     REQUIRE_THROWS_AS(arr1.at(11), std::out_of_range);
+
+    Array arr2 = {3.14, 6.65, 0.14}; // Array<double, 3>
+}
+
+////////////////////////////////////////////////////////////
+
+template <typename T1, typename T2>
+struct Pair
+{
+    T1 first;
+    T2 second;
+
+    //template <typename U1, typename U2>
+    //Pair(U1&& f, U2&& s)
+    //    : first{std::forward<U1>(f)}
+    //    , second{std::forward<U2>(s)}
+    //{
+    //}
+
+    template <typename U1, typename U2>
+    Pair(U1&& f, U2&& s);        
+};
+
+template <typename T1, typename T2>
+    template <typename U1, typename U2>
+Pair<T1, T2>::Pair(U1&& f, U2&& s)
+    : first{std::forward<U1>(f)}
+    , second{std::forward<U2>(s)}
+{
+}
+
+// deduction guide
+template <typename T1, typename T2>
+Pair(T1, T2) -> Pair<T1, T2>;
+
+template <typename T1, typename T2>
+Pair<T1, T2> my_make_pair(T1 f, T2 s)
+{
+    return Pair<T1, T2>(f, s);
+}
+
+TEST_CASE("Pair")
+{
+    Pair<int, double> p1{1, 3.14};
+
+    p1 = my_make_pair(4, 6.66);
+
+    CHECK(p1.first == 4);
+    CHECK(p1.second == 6.66);
+
+    // Class-Template Argument Deduction - CTAD
+    Pair p3{1, 4.44};         // Pair<int, double>
+    Pair p4{"text", "text"s}; // Pair<const char*, std::string> p4
 }
