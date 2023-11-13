874319fab7f2567517992a5e29b694e77715afeb
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-11-13 12:03:45 +0100

    SFINAE

--- sfinae/sfinae.cpp
+++ sfinae/sfinae.cpp
@@ -5,7 +5,23 @@
 
 using namespace std::literals;
 
+#ifdef _MSC_VER
+#define __PRETTY_FUNCTION__ __FUNCSIG__
+#endif
+
+template <typename T>
+void foo(T obj)
+{
+    std::cout << __PRETTY_FUNCTION__ << "\n";
+}
+
+template <typename T>
+void foo(T* ptr)
+{
+    std::cout << __PRETTY_FUNCTION__ << "\n";
+}
+
 TEST_CASE("SFINAE")
 {
-    CHECK(true);
+    foo(42);
 }
