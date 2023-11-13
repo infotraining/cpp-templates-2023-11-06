ad3264aa496914e0fb450892ccc260b541d98cc2
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-11-13 11:53:11 +0100

    type traits - 2

--- type-traits/type_traits.cpp
+++ type-traits/type_traits.cpp
@@ -47,11 +47,95 @@ TEST_CASE("type traits - RemoveConst")
 
 ////////////////////////////////////////////////////////////////
 
-// TODO
+template<typename T>
+struct RemoveRef
+{
+    using type = T;
+};
+
+template<typename T>
+struct RemoveRef<T&>
+{
+    using type = T;
+};
+
+template <typename T>
+struct RemoveRef<T&&>
+{
+    using type = T;
+};
+
+template<typename T>
+using RemoveRef_t = typename RemoveRef<T>::type;
 
 TEST_CASE("type traits - RemoveRef")
 {
     static_assert(std::is_same_v<RemoveRef_t<int&>, int>);
-    static_assert(std::is_same_v<RemoveRef_t<const std::vector<int>&, const std::vector<int>>);
-    static_assert(std::is_same_v<RemoveRef_t<std::vector<int>&&, std::vector<int>>);
+    static_assert(std::is_same_v<RemoveRef_t<const std::vector<int>&>, const std::vector<int>>);
+    static_assert(std::is_same_v<RemoveRef_t<std::vector<int>&&>, std::vector<int>>);
+
+    using T = int&;
+
+    RemoveRef_t<T> obj{};  // int obj;
 }
+
+//////////////////////////////////////////////////////////////////
+
+template <typename T, T v>
+struct IntegralConstant 
+{
+    static constexpr T value = v;
+};
+
+template <typename T, T v>
+constexpr T IntegralConstant_v = IntegralConstant<T, v>::value;
+
+TEST_CASE("type traits - IntegralConstant")
+{
+    static_assert(IntegralConstant<int, 5>::value == 5);
+    static_assert(IntegralConstant_v<int, 5> == 5);
+}
+
+template <bool v>
+using BoolConstant = IntegralConstant<bool, v>;
+
+using TrueType = BoolConstant<true>;
+using FalseType = BoolConstant<false>;
+
+////////////////////////////////////////////////////////////////////
+
+template <typename T>
+struct IsVoid : std::false_type
+{
+    //static constexpr bool value = false;
+};
+
+template <>
+struct IsVoid<void> : std::true_type
+{
+    //static constexpr bool value = true;
+};
+
+template <typename T>
+constexpr bool IsVoid_v = IsVoid<T>::value;
+
+TEST_CASE("type trait - IsVoid")
+{
+    static_assert(IsVoid<int>::value == false);
+    static_assert(IsVoid_v<void> == true);
+}
+
+///////////////////////////////////////////////////////////////////
+
+template <typename T>
+struct IsPointer : std::false_type{};
+ 
+template <typename T>
+struct IsPointer<T*> : std::true_type{};
+ 
+template <typename T>
+constexpr bool IsPointer_v = IsPointer<T>::value;
+ 
+static_assert(IsPointer<int*>::value);
+static_assert(IsPointer_v<const int*>);
+static_assert(IsPointer_v<const int> == false);
