865fff1a6f5c903428629dd922e93ae7e36523cc
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-11-13 13:56:59 +0100

    void_t

--- sfinae/sfinae.cpp
+++ sfinae/sfinae.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
 #include <string>
@@ -62,17 +63,30 @@ auto do_stuff(const T& obj) -> std::enable_if_t<(sizeof(T) > 8)> // for big obje
 namespace AlternativeTake
 {
     template <typename T, typename = std::enable_if_t<(sizeof(T) <= 8)>>
-    void do_stuff(T obj)  // for small objects
+    void do_stuff(T obj) // for small objects
     {
         std::cout << __PRETTY_FUNCTION__ << "\n";
     }
 
-    template <typename T, typename = void, typename = std::enable_if_t<(sizeof(T) > 8)>>
+    template <typename T, typename = void, typename = std::enable_if_t<(sizeof(T) > 8 && sizeof(T) <= 16)>>
     void do_stuff(const T& obj)
     {
         std::cout << __PRETTY_FUNCTION__ << "\n";
     }
+
+    template <typename T, typename = void, typename = std::enable_if_t<(sizeof(T) > 16)>>
+    void do_stuff(std::shared_ptr<T> ptr)
+    {
+        std::cout << __PRETTY_FUNCTION__ << "\n";
+    }
 } // namespace AlternativeTake
+
+struct Data
+{
+    int values[100] = {};
+
+    Data() = default;
+};
 
 TEST_CASE("SFINAE + enable_if")
 {
@@ -81,4 +95,97 @@ TEST_CASE("SFINAE + enable_if")
     std::vector<int> vec = {1, 2, 3, 4};
 
     AlternativeTake::do_stuff(vec);
+
+    AlternativeTake::do_stuff(std::make_shared<Data>());
+}
+
+/////////////////////////////////////////////////////////////////////////
+
+template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
+bool is_power_of_2(T value)
+{
+    return value > 0 && (value & (value - 1)) == 0;
+}
+
+template <typename T>
+auto is_power_of_2(T value) -> std::enable_if_t<std::is_floating_point_v<T>, bool>
+{
+    int exponent;
+    const T mantissa = std::frexp(value, &exponent);
+    return mantissa == T(0.5);
+}
+
+TEST_CASE("is power of 2")
+{
+    CHECK(is_power_of_2(512));
+    CHECK(is_power_of_2(4u));
+    CHECK(is_power_of_2(9ull) == false);
+
+    CHECK(is_power_of_2(16.0));
+    CHECK(is_power_of_2(512.0f));
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+template <typename T, typename Enabled = void>
+struct DataSet
+{
+    std::array<T, 64> data = {};
+
+    void info() const
+    {
+        std::cout << "Data with std::array!\n";
+    }
+};
+
+template <typename T>
+struct DataSet<T, std::enable_if_t<std::is_floating_point_v<T>>>
+{
+    std::vector<T> data = {};
+
+    void info() const
+    {
+        std::cout << "Data with std::vector!\n";
+    }
+};
+
+TEST_CASE("Data + enable_if")
+{
+    DataSet<int> d1;
+    d1.info();
+
+    DataSet<double> d2;
+    d2.info();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename, typename = void>
+constexpr bool is_iterable{}; // false
+
+template <typename T>
+constexpr bool is_iterable<T, std::void_t<decltype(std::declval<T>().begin()), decltype(std::declval<T>().end())>>
+    = true;
+
+
+template <typename T, typename = std::enable_if_t<!is_iterable<T>>>
+void print(const T& value)
+{
+    std::cout << "value: " << value << "\n";   
+}
+
+template <typename T, typename = void, typename = std::enable_if_t<is_iterable<T>>>
+void print(const T& rng)
+{
+    std::cout << "values: ";
+    for(const auto& item : rng)
+        std::cout << item << " ";
+    std::cout << "\n";
+}
+
+TEST_CASE("print")
+{
+    print(42);
+
+    print(std::vector{1, 2, 3});
 }
