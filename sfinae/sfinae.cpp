3bfa4d4e708ce87476baab9d2d7a0860ae7dcd6f
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-11-20 11:43:32 +0100

    Sfinae replaced by concepts

--- sfinae/sfinae.cpp
+++ sfinae/sfinae.cpp
@@ -49,21 +49,21 @@ static_assert(std::is_same_v<EnableIf_t<true, int>, int>);
 
 ////////////////////////////////////////////////////////////////////////
 
+template<typename T>
+concept BigObject = requires {
+    requires sizeof(T) > 8; 
+};
+
 template <typename T>
-auto do_stuff(T obj) -> std::enable_if_t<(sizeof(T) <= 8)> // for small objects
+requires (!BigObject<T>)
+void do_stuff(T obj) //-> std::enable_if_t<(sizeof(T) <= 8)> // for small objects
 {
     std::cout << __PRETTY_FUNCTION__ << "\n";
 }
 
-template <typename T>
-auto do_stuff(const T& obj) -> std::enable_if_t<(sizeof(T) > 8)> // for big objects
+void do_stuff(BigObject auto const& obj) //-> std::enable_if_t<(sizeof(T) > 8)> // for big objects
 {
     std::cout << __PRETTY_FUNCTION__ << "\n";
-}
-
-namespace Cpp20
-{
-    
 }
 
 namespace AlternativeTake
@@ -99,14 +99,14 @@ TEST_CASE("SFINAE + enable_if")
 
 /////////////////////////////////////////////////////////////////////////
 
-template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
+template <std::integral T>
 bool is_power_of_2(T value)
 {
     return value > 0 && (value & (value - 1)) == 0;
 }
 
-template <typename T>
-auto is_power_of_2(T value) -> std::enable_if_t<std::is_floating_point_v<T>, bool>
+template <std::floating_point T>
+auto is_power_of_2(T value)
 {
     int exponent;
     const T mantissa = std::frexp(value, &exponent);
@@ -125,7 +125,7 @@ TEST_CASE("is power of 2")
 
 ///////////////////////////////////////////////////////////////////////////
 
-template <typename T, typename Enabled = void>
+template <typename T>
 struct DataSet
 {
     std::array<T, 64> data = {};
@@ -136,8 +136,8 @@ struct DataSet
     }
 };
 
-template <typename T>
-struct DataSet<T, std::enable_if_t<std::is_floating_point_v<T>>>
+template <std::floating_point T>
+struct DataSet<T>
 {
     std::vector<T> data = {};
 
@@ -164,13 +164,19 @@ constexpr bool is_iterable{}; // false
 template <typename T>
 constexpr bool is_iterable<T, std::void_t<decltype(std::declval<T>().begin()), decltype(std::declval<T>().end())>> = true;
 
-template <typename T, typename = std::enable_if_t<!is_iterable<T>>>
+template <typename T>
 void print(const T& value)
 {
     std::cout << "value: " << value << "\n";   
 }
 
-template <typename T, typename = void, typename = std::enable_if_t<is_iterable<T>>>
+template<typename T>
+concept Iterable = requires(T iter) {
+    { std::begin(iter) } -> std::input_or_output_iterator;
+    { std::end(iter) } -> std::input_or_output_iterator;
+};
+
+template <Iterable T>
 void print(const T& rng)
 {
     std::cout << "values: ";
--- traits-and-policies/traits_and_policies.cpp
+++ traits-and-policies/traits_and_policies.cpp
@@ -1,11 +1,259 @@
+#include <algorithm>
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
-#include <string>
+#include <numeric>
 #include <vector>
 
 using namespace std::literals;
 
-TEST_CASE("traits and policies")
+namespace Step1
 {
-    CHECK(true);
+    template <typename T>
+    T accumulate(const T* begin, const T* end)
+    {
+        T total = T{};
+
+        while (begin != end)
+        {
+            total += *begin;
+            ++begin;
+        }
+
+        return total;
+    }
+} // namespace Step1
+
+TEST_CASE("accumulate - 1")
+{
+    using std::begin, std::end;
+
+    uint8_t data[255];
+
+    std::iota(begin(data), end(data), 1);
+
+    auto result = Step1::accumulate(begin(data), end(data));
+
+    // CHECK(result == 32640);  // should be 1 + 2 + 3 + ... + 255 = 32640 (uint8_t overflow)
+}
+
+namespace Step2
+{
+    template <typename T>
+    struct AccumulationTraits;
+
+    template <>
+    struct AccumulationTraits<uint8_t>
+    {
+        typedef uintmax_t AccumulatorType;
+    };
+
+    template <>
+    struct AccumulationTraits<float>
+    {
+        typedef double AccumulatorType;
+    };
+
+    template <typename T>
+    typename AccumulationTraits<T>::AccumulatorType accumulate(const T* begin, const T* end)
+    {
+        using AccT = typename AccumulationTraits<T>::AccumulatorType;
+
+        AccT total = T{};
+
+        while (begin != end)
+        {
+            total += *begin;
+            ++begin;
+        }
+
+        return total;
+    }
+} // namespace Step2
+
+TEST_CASE("accumulate - 2")
+{
+    using std::begin, std::end;
+
+    uint8_t data[255];
+
+    std::iota(begin(data), end(data), 1);
+
+    auto result = Step2::accumulate(begin(data), end(data));
+
+    CHECK(result == 32640); // should be 1 + 2 + 3 + ... + 255 = 32640
+}
+
+namespace Step3
+{
+    template <typename T>
+    struct AccumulationTraits;
+
+    template <>
+    struct AccumulationTraits<uint8_t>
+    {
+    public:
+        typedef int AccumulatorType;
+        static constexpr AccumulatorType const zero = 0;
+    };
+
+    template <>
+    struct AccumulationTraits<int>
+    {
+    public:
+        typedef long AccumulatorType;
+        static constexpr AccumulatorType zero = 0;
+    };
+
+    template <typename T>
+    typename AccumulationTraits<T>::AccumulatorType accumulate(T const* begin, T const* end)
+    {
+        using AccT = typename AccumulationTraits<T>::AccumulatorType;
+
+        AccT total = AccumulationTraits<T>::zero; // usint zero defined in a traits class
+
+        while (begin != end)
+        {
+            total += *begin;
+            ++begin;
+        }
+
+        return total;
+    }
+} // namespace Step3
+
+TEST_CASE("accumulate - 3")
+{
+    using std::begin, std::end;
+
+    uint8_t data[255];
+
+    std::iota(begin(data), end(data), 1);
+
+    auto result = Step3::accumulate(begin(data), end(data));
+
+    CHECK(result == 32640); // should be 1 + 2 + 3 + ... + 255 = 32640
+}
+
+namespace Step4
+{
+    template <typename T>
+    struct AccumulationTraits;
+
+    template <>
+    struct AccumulationTraits<uint8_t>
+    {
+    public:
+        typedef unsigned int AccumulatorType;
+        static constexpr AccumulatorType const zero = 0;
+    };
+
+    template <>
+    struct AccumulationTraits<int>
+    {
+    public:
+        typedef long AccumulatorType;
+        static constexpr AccumulatorType zero = 0;
+    };
+
+    template <typename T, typename Traits = AccumulationTraits<T>>
+    typename Traits::AccumulatorType accumulate(T const* begin, T const* end)
+    {
+        using AccT = typename Traits::AccumulatorType;
+
+        AccT total = Traits::zero;
+
+        while (begin != end)
+        {
+            total += *begin;
+            ++begin;
+        }
+
+        return total;
+    }
+} // namespace Step4
+
+TEST_CASE("accumulate - 4")
+{
+    using std::begin, std::end;
+
+    uint8_t data[255];
+
+    std::iota(begin(data), end(data), 1);
+
+    auto result = Step4::accumulate(begin(data), end(data));
+
+    CHECK(result == 32640); // should be 1 + 2 + 3 + ... + 255 = 32640 (uint8_t overflow)
+}
+
+inline namespace Step5
+{
+    template <typename T>
+    struct AccumulationTraits;
+
+    template <>
+    struct AccumulationTraits<uint8_t>
+    {
+        typedef unsigned int AccumulatorType;
+        static constexpr AccumulatorType const zero = 0;
+    };
+
+    template <>
+    struct AccumulationTraits<int>
+    {
+        typedef long AccumulatorType;
+        static constexpr AccumulatorType zero = 0;
+    };
+
+    struct SumPolicy
+    {
+        template <typename T1, typename T2>
+        static void accumulate(T1& total, const T2& value)
+        {
+            total += value;
+        }
+    };
+
+    template <typename T, typename AccumulationPolicy = SumPolicy, typename Traits = AccumulationTraits<T>>
+    typename Traits::AccumulatorType accumulate(T const* begin, T const* end)
+    {
+        using AccT = typename Traits::AccumulatorType;
+
+        AccT total = Traits::zero;
+
+        while (begin != end)
+        {
+            AccumulationPolicy::accumulate(total, *begin);
+            ++begin;
+        }
+
+        return total;
+    }
+} // namespace Step5
+
+struct MultiplyAccumulationTraits
+{
+    typedef unsigned int AccumulatorType;
+    static constexpr AccumulatorType const zero = 1;
+};
+
+struct MultiplyPolicy
+{
+    template <typename T1, typename T2>
+    static void accumulate(T1& total, const T2& value)
+    {
+        total *= value;
+    }
+};
+
+TEST_CASE("accuumulate - 5")
+{
+    using namespace std;
+
+    uint8_t data[255];
+
+    iota(begin(data), end(data), 1);
+
+    auto result = Step5::accumulate(begin(data), end(data));
+
+    cout << "result: " << static_cast<int>(result) << endl;
 }
